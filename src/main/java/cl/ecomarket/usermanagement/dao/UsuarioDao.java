7de9112286bad8204f0627e41eb173607c4cde62
Naglock <iv.huentemilla@duocuc.cl>
2025-05-25 00:51:33 -0400

    se elimino una linea de codigo que estaba de mas

--- src/main/java/cl/ecomarket/usermanagement/dao/UsuarioDao.java
+++ src/main/java/cl/ecomarket/usermanagement/dao/UsuarioDao.java
@@ -1,8 +1,5 @@
 package cl.ecomarket.usermanagement.dao;
 
-
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import cl.ecomarket.usermanagement.model.Usuario;
